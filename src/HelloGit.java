06e45f27b5f77b4fd7669e64df3f1e4cd6159887
yuanchengliang <yuanchengliang@zongheng.com>
2021-10-20 23:55:30 +0800

    yclcbg commit1

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,7 +3,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello git");
+		System.out.println("hello git222");
 		System.out.println("新增333");
 		System.out.println("新增444");
 	}
